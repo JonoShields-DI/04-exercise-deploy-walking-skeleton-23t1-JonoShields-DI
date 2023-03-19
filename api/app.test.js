e301eed58ebab4d59c0b7c5215dc08d013c4dab5
Jono Shields <jono.shields@developersinstitute.ac.nz>
2023-03-20 10:33:13 +1300

    Add tests for api and client

---
+++ api/app.test.js
@@ -0,0 +1,8 @@
+describe("Test", () => {
+    describe("First test", () => {
+        test("1 + 1 = 2", () => {
+            const answer = 1 + 1
+            expect(answer).toBe(2)
+        })
+    })
+})
